500306a32303c4d4390a8df6da951118ea201167
EpicRock <44951561+EpicRock@users.noreply.github.com>
2018-11-21 19:56:16 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let functionEnd = false;
 let functionReturn;
+let isChangingtable = false;
 
 function getElement (multipleElements) {
 	let returnArr = [];
-	$("#elements-table").css("transform", "scale(1.03, 1.03)");
+	$("#table-container").css("transform", "scale(1.03, 1.03)");
 	$(".cover").css("backgroundColor", "rgba(0, 0, 0, 0.25)");
-	$(".message-box").css("top", $("#elements-table").offset().top + $("#elements-table").height() + 50 + "px");
+	$(".cover").css("pointerEvents", "initial");
+	$(".message-box").css("top", $("#under-table").offset().top + $("#under-table").height() + 50 + "px");
 	$(".message-box").fadeIn(700);
 	if (multipleElements) {
 		$(".message-box").text("Wybierz pierwiastki, a potem naciśnij ten napis");
@@ -13,14 +15,15 @@ function getElement (multipleElements) {
 		$(".message-box").on("click.getElement", () => {
 			$(".message-box").fadeOut(700);
 			$(".cover").css("backgroundColor", "rgba(0, 0, 0, 0)");
-			$("#elements-table").css("transform", "scale(1, 1)");
-			$("#elements-table").children("div").off(".getElement");
+			$(".cover").css("pointerEvents", "none");
+			$("#table-container").css("transform", "scale(1, 1)");
+			$(".elements-container").children("div").off(".getElement");
 			$(".message-box").off(".getElement");
 			$(".message-box").css("cursor", "initial");
-			$("#elements-table").children("div").each(function () {
+			$(".elements-container").children("div").each(function () {
 				$(this).data("clicked", false);
 				$(this).css("transform", "scale(1, 1)");
-				$(this).css("boxShadow", "0 0 0px 0px blue");
+				$(this).css("boxShadow", "0 0 0px 0px transparent");
 				$(this).css("backgroundColor", "");
 			});
 			functionEnd = true
@@ -30,21 +33,21 @@ function getElement (multipleElements) {
 	else {
 		$(".message-box").text("Wybierz pierwiastek");
 	}
-	$("#elements-table").children("div").on("mouseenter.getElement", (event) => {
+	$(".elements-container").children("div").on("mouseenter.getElement", (event) => {
 		const element = $(event.currentTarget);
 		element.css("transform", "scale(1.03, 1.03)");
-		element.css("boxShadow", "0 0 7px 3px blue");
+		element.css("boxShadow", "#00639c 0px 0px 7px 3px");
 		element.css("backgroundColor", "white");
 	});
-	$("#elements-table").children("div").on("mouseleave.getElement", (event) => {
+	$(".elements-container").children("div").on("mouseleave.getElement", (event) => {
 		const element = $(event.currentTarget);
 		if (element.data("clicked") === false || element.data("clicked") === undefined) {
 			element.css("transform", "scale(1, 1)");
-			element.css("boxShadow", "0 0 0px 0px blue");
+			element.css("boxShadow", "0 0 0px 0px transparent");
 			element.css("backgroundColor", "");
 		}
 	});
-	$("#elements-table").children("div").on("click.getElement", (event) => {
+	$(".elements-container").children("div").on("click.getElement", (event) => {
 		const element = $(event.currentTarget);
 		if (multipleElements) {
 			if (element.data("clicked") === undefined) {
@@ -66,12 +69,13 @@ function getElement (multipleElements) {
 		}
 		else {
 			element.css("transform", "scale(1, 1)");
-			element.css("boxShadow", "0 0 0px 0px blue");
+			element.css("boxShadow", "0 0 0px 0px transparent");
 			element.css("backgroundColor", "");
 			$(".message-box").fadeOut(700);
 			$(".cover").css("backgroundColor", "rgba(0, 0, 0, 0)");
-			$("#elements-table").css("transform", "scale(1, 1)");
-			$("#elements-table").children("div").off(".getElement");
+			$(".cover").css("pointerEvents", "none");
+			$("#table-container").css("transform", "scale(1, 1)");
+			$(".elements-container").children("div").off(".getElement");
 			functionEnd = true;
 			functionReturn = element;
 		}
@@ -116,15 +120,47 @@ $(document).ready(() => {
 		}, 100);
 	});
 	$(".changeTable").click(() => {
-		if ($(".logo").eq(1).css("opacity") === "0") {
-			$(".logo").eq(1).css("opacity", 1);
-			$("#left").css("left", "-100%");
-			$("#right").css("left", "0%");
-		}
-		else {
-			$(".logo").eq(1).css("opacity", 0);
-			$("#left").css("left", "0%");
-			$("#right").css("left", "100%")
+		//block clicking in moveing phase
+		if (!isChangingtable) {
+			isChangingtable = true;
+			let viewingRight;
+			
+			setTimeout(() => {
+				isChangingtable = false;
+			}, 2000);
+			$(".changeTable").animate({
+				opacity: 0
+			}, 1000, () => {
+				if (viewingRight) {
+					$(".changeTable").children("p").text("Przejdź do tablicy Mendelejewa");
+					$(".changeTable img").eq(0).show();
+					$(".changeTable img").eq(1).hide();
+				}
+				else {
+					$(".changeTable").children("p").text("Przejdź do tablicy rozpuszczalności");
+					$(".changeTable img").eq(1).show();
+					$(".changeTable img").eq(0).hide();
+				}
+				$(".changeTable").animate({
+					opacity: 1
+				}, 1000);
+			});
+			if ($(".logo").eq(1).css("opacity") === "0") {
+				viewingRight = true;
+				$(".logo").eq(1).css("opacity", 1);
+				$("#left").css("left", "-100%");
+				$("#right").css("left", "0%");
+				$("#top-bar").css("backgroundColor", "#8ca77b");
+				$("#top-bar").css("borderBottom", "3px solid #205200");
+			}
+			else {
+				viewingRight = false;
+				$(".logo").eq(1).css("opacity", 0);
+				$("#left").css("left", "0%");
+				$("#right").css("left", "100%");
+				$("#top-bar").css("backgroundColor", "#819da0");
+				$("#top-bar").css("borderBottom", "3px solid #005d67");
+			}
 		}
 	});
 });
